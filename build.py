b16af66fa006bb6185cd6b4593c8c16e21af7237
Iain Dawson <iain@colons.co>
2014-05-13 19:45:18 +0100

    Set up a structure for a static blog build system.

---
+++ build.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import os
+import shutil
+import subprocess
+
+from django.template import Context
+from django.template.loader import get_template
+from django.conf import settings
+
+
+BASE_PATH = os.path.dirname(__file__)
+BUILD_PATH = os.path.join(BASE_PATH, '_build')
+ARTICLES_PATH = os.path.join(BASE_PATH, 'articles')
+
+settings.configure(
+    TEMPLATE_DIRS=[os.path.join(BASE_PATH, 'templates')],
+)
+
+
+class Article(object):
+    def __init__(self, slug):
+        self.slug = slug
+        self.path = os.path.join(ARTICLES_PATH, self.slug)
+        self.markdown_path = os.path.join(self.path, 'article.markdown')
+
+    def get_html(self):
+        output = subprocess.check_output(['markdown', self.markdown_path])
+        return output
+
+    def render(self):
+        context = Context({
+            'content': self.get_html()
+        })
+
+        return get_template('article.html').render(context)
+
+    def bounce(self):
+        build_directory = os.path.join(BUILD_PATH, self.slug)
+        os.mkdir(build_directory)
+        html_path = os.path.join(build_directory, 'index.html')
+
+        rendered = self.render()
+
+        with open(html_path, 'w') as html_file:
+            html_file.write(rendered.encode('utf-8'))
+
+
+def build():
+    if os.path.isdir(BUILD_PATH):
+        shutil.rmtree(BUILD_PATH)
+
+    os.mkdir(BUILD_PATH)
+
+    for slug in os.listdir(ARTICLES_PATH):
+        article = Article(slug)
+        article.bounce()
+
+
+if __name__ == "__main__":
+    build()
